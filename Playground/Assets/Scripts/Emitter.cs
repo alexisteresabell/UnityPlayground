f57551fd13072601f0c2c1e9d120f02a6a7698e7
Alexis Bell <gadgetgirlgames@gmail.com>
2019-04-07 15:02:42 -0600

    Basics of Pooler set up.

---
+++ Playground/Assets/Scripts/Emitter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public float velocity;
+    public float timeBetweenSpawns;
+
+    float timeSinceLastSpawn;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnGuest();
+        }
+    }
+
+    void SpawnGuest()
+    {
+        GameObject spawn = ObjectPooler.Pool.GetObject();
+        if (spawn != null)
+        {
+            spawn.transform.position = transform.position;
+            Rigidbody body = spawn.GetComponent<Rigidbody>();
+            body.velocity = transform.forward * velocity;
+        }
+    }
+}
---
+++ Playground/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    public static ObjectPooler Pool { get; private set; }
+
+    public GameObject prefab;
+    List<GameObject> pooledObjects = new List<GameObject>();
+    public int objectsInPool = 5;
+    public bool canGrow = true;
+
+    void Awake()
+    {
+        if (Pool == null)
+        {
+            Pool = this;
+        }
+        else if (this != Pool)
+        {
+            Destroy(this.gameObject);
+        }
+        CreatePool();
+    }
+
+    private void CreatePool()
+    {
+
+        for (int i = 0; i < objectsInPool; i++)
+        {
+            GameObject newObject = Instantiate<GameObject>(prefab);
+            newObject.gameObject.SetActive(false);
+            newObject.transform.SetParent(transform);
+            newObject.transform.position = this.transform.position;
+            pooledObjects.Add(newObject);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        GameObject newObject = null;
+        foreach (GameObject g in pooledObjects)
+        {
+            if (!g.gameObject.activeSelf)
+            {
+                newObject = g;
+                newObject.gameObject.SetActive(true);
+                break;
+            }
+        }
+
+        if (newObject == null && canGrow)
+        {
+            //we need to make a new guest
+            newObject = Instantiate<GameObject>(prefab);
+            newObject.transform.SetParent(transform);
+            pooledObjects.Add(newObject);
+        }
+
+        if (newObject == null)
+        {
+            Debug.Log("All objects are active, pool can't grow");
+        }
+
+        return newObject;
+    }
+
+    public void ReturnToPool(GameObject go)
+    {
+        go.transform.position = this.transform.position;
+        go.gameObject.SetActive(false);
+    }
+
+    public void RemoveFromPool(GameObject go)
+    {
+        pooledObjects.Remove(go);
+    }
+}
+
