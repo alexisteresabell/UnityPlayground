3b0d8d513ca4592a0e82281b3e59970915ca19db
Alexis Bell <gadgetgirlgames@gmail.com>
2019-04-07 14:42:40 -0600

    Set up walls. Can make balls of random colors.

---
+++ Playground/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public List<Material> myMats = new List<Material>();
+    public Material material;
+    public MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        if (material == null)
+        {
+            material = new Material(SetRandomColor());
+            meshRenderer.materials[0].CopyPropertiesFromMaterial(material);
+
+        }
+    }
+
+    private void Start()
+    {
+        SetRandomColor();
+    }
+
+    public Material SetRandomColor()
+    {
+        int index = Random.Range(0, myMats.Count);
+        return myMats[index];
+    }
+
+}
---
+++ Playground/Assets/Scripts/BaseColor.cs
@@ -0,0 +1,11 @@
+﻿public enum BaseColor
+{
+    WHITE,
+    BLACK,
+    RED,
+    ORANGE,
+    YELLOW,
+    BLUE,
+    GREEN,
+    PURPLE,
+}
